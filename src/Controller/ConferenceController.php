58f0f48b4f091b769fd53339da2080d0d0245819
Scott Collier <scott@onlinespaces.com>
2020-06-14 17:25:28 +0000

    Caching, group routes, etc.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -55,11 +55,33 @@ class ConferenceController extends AbstractController
      */
     public function index(ConferenceRepository $conferenceRepository)
     {
-        return new Response(
+        $response = new Response(
             $this->twig->render('conference/index.html.twig', [
                 'conferences' => $conferenceRepository->findAll(),
             ])
         );
+
+        $response->setSharedMaxAge(3600);
+        return $response;
+    }
+
+    /**
+     * @Route("/conference_header", name="conference_header")
+     *
+     * @param ConferenceRepository $conferenceRepository
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function conferenceHeader(ConferenceRepository $conferenceRepository)
+    {
+        $response = new Response($this->twig->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+
+        $response->setSharedMaxAge(3600);
+        return $response;
     }
 
     /**
--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -6,7 +6,12 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 class CommentMessageHandler implements MessageHandlerInterface
 {
@@ -20,15 +25,57 @@ class CommentMessageHandler implements MessageHandlerInterface
     private $commentRepository;
 
     /**
+     * @var MessageBusInterface $bus
+     */
+    private $bus;
+
+    /**
+     * @var WorkflowInterface $workflow
+     */
+    private $workflow;
+
+    /**
+     * @var LoggerInterface|null $logger
+     */
+    private $logger;
+
+    /**
+     * @var MailerInterface $mailer
+     */
+    private $mailer;
+
+    /**
+     * @var string $adminEmail
+     */
+    private $adminEmail;
+
+    /**
      * @param EntityManagerInterface $entityManager
      * @param SpamChecker $spamChecker
      * @param CommentRepository $commentRepository
+     * @param MessageBusInterface $bus
+     * @param WorkflowInterface $commentStateMachine
+     * @param LoggerInterface|null $logger
      */
-    public function __construct(EntityManagerInterface $entityManager, SpamChecker $spamChecker, CommentRepository $commentRepository)
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        SpamChecker $spamChecker,
+        CommentRepository $commentRepository,
+        MessageBusInterface $bus,
+        WorkflowInterface $commentStateMachine,
+        MailerInterface $mailer,
+        string $adminEmail,
+        LoggerInterface $logger = null
+    )
     {
         $this->entityManager = $entityManager;
         $this->spamChecker = $spamChecker;
         $this->commentRepository = $commentRepository;
+        $this->bus = $bus;
+        $this->workflow = $commentStateMachine;
+        $this->mailer = $mailer;
+        $this->adminEmail = $adminEmail;
+        $this->logger = $logger;
     }
 
     /**
@@ -47,13 +94,43 @@ class CommentMessageHandler implements MessageHandlerInterface
             return;
         }
 
-        if (2 === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
-            $comment->setState('spam');
-        } else {
-            $comment->setState('published');
+//        if (2 === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
+//            $comment->setState('spam');
+//        } else {
+//            $comment->setState('published');
+//        }
+
+        if ($this->workflow->can($comment, 'accept')) {
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+            $transition = 'accept';
+
+            if ($score === 2) {
+                $transition = 'reject_spam';
+            } elseif ($score === 1) {
+                $transition = 'might_be_spam';
+
+                $this->workflow->apply($comment, $transition);
+                $this->entityManager->flush();
+
+                $this->bus->dispatch($message);
+            } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
+//                $this->workflow->apply($comment, $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
+//                $this->entityManager->flush();
+                $this->mailer->send((new NotificationEmail())
+                    ->subject('New comment posted')
+                    ->htmlTemplate('emails/comment_notification.html.twig')
+                    ->from($this->adminEmail)
+                    ->to($this->adminEmail)
+                    ->context(['comment' => $comment])
+                );
+            } elseif ($this->logger) {
+                $this->logger->debug('Dropping the comment message', [
+                    'comment' => $comment->getId(),
+                    'state' => $comment->getState(),
+                ]);
+            }
         }
 
-        $this->entityManager->flush();
     }
 
 }
